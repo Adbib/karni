f184cc71c8eea48f98b67c2236023f3e12c1e38e
Adbib <mradbib@gmail.com>
2022-04-07 01:25:17 +0000

    git

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
+import Terms from "./pages/Terms";
+import Privacy from "./pages/Privacy";
 function App() {
   return (
     <>
@@ -11,6 +13,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/terms" element={<Terms />} />
+          <Route path="/privacy" element={<Privacy />} />
         </Routes>
       </BrowserRouter>
     </>
